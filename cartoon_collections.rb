b4b5fb1c72bd217eff0477c2a245dd9e5de5f56f
Anuradha Khambete <anukhambete@gmail.com>
2018-04-12 21:08:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,13 +14,18 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = cheese_types.length - 1
-  j = array.length - 1
   
     while i > 0 
+    j = array.length - 1
+    
       while j > 0 
         if cheese_types[i]==array[j]
           return array[i]
         else
           nil
         end
+        j = j - 1
+      end
+      i = i - 1
+    end
 end
